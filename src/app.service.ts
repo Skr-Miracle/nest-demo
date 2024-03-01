039cb5a620d4f5e636a186de0e1f915095aa09fd
“skr111” <526206383@qq.com>
2024-03-01 09:32:47 +0800

    dist

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,18 +9,19 @@ import {
   Query,
   Param,
 } from '@nestjs/common';
+import { find } from 'rxjs';
 
 @Controller('cats')
 //@Controller({ host: ':account.example.com' })
 // export class AccountController {
 //   @Get()
-//   getInfo(@HostParam('account') account: string) {
+//   getInfo(@HostParam('account') account: string) {i
 //     return account;
 //   }
 // }
 //与路由 path 类似，hosts 选项可以使用标记来捕获主机名中该位置的动态值。下面 @Controller() 装饰器示例中的主机参数令牌演示了这种用法。可以使用 @HostParam() 装饰器访问以这种方式声明的主机参数，应将其添加到方法签名中。
 //@Controller({ host: 'admin.example.com' })
-//  @Controller 装饰器可以采用 host 选项来要求传入请求的 HTTP 主机匹配某个特定值
+//@Controller 装饰器可以采用 host 选项来要求传入请求的 HTTP 主机匹配某个特定值
 export class CatsControll {
   // 'ab*cd' 路由路径将匹配 abcd、ab_cd、abecd 等。字符 ?、+、* 和 () 可以在路由路径中使用，并且是它们对应的正则表达式的子集。连字符 (-) 和点 (.) 由基于字符串的路径逐字解释
   @Get('ab*cd')
@@ -58,9 +59,14 @@ export class CatsControll {
   //当你需要接受动态数据作为请求的一部分时（例如，GET /cats/1 获取 ID 为 1 的 cat），具有静态路径的路由将不起作用。为了定义带参数的路由，我们可以在路由的路径中添加路由参数标记，以捕获请求 URL 中该位置的动态值。下面 @Get() 装饰器示例中的路由参数令牌演示了这种用法。可以使用 @Param() 装饰器访问以这种方式声明的路由参数，应将其添加到方法签名中
   //@Param() 用于修饰方法参数（上例中的 params），并使路由参数可用作方法体内该修饰方法参数的属性。如上面的代码所示，我们可以通过引用 params.id 来访问 id 参数。也可以传入一个特定的参数 token 给装饰器，然后在方法体中直接通过名称引用路由参数
   @Get(':id')
+  //异步形
   findOne(@Param() params: any): string {
     console.log(params.id);
     return `This action returns a #${params.id} cat`;
+  }
+  //每个异步函数必须返回Promise
+  async findTwo(): Promise<any[]> {
+    return [];
   }
   //带参数的路由应在任何静态路径之后声明。这可以防止参数化路径拦截发往静态路径的流量。
 }
